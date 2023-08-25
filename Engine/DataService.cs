2f596f3ecccf0a90489b6372970d656f729ba693
Rob Davis <bfe5e925@opayq.com>
2023-08-25 11:01:31 +1000

    Initial commit

---
+++ Engine/DataService.cs
@@ -0,0 +1,36 @@
+﻿using System.Data;
+
+namespace Engine
+{
+    public class DataService
+    {
+        private readonly DataTable _data = new();
+        public event EventHandler? DataChanged;
+
+        public DataService()
+        {
+            _data.Columns.Add("IsError");
+            _data.Columns.Add("Value");
+
+            RefreshData(10);
+        }
+
+        public DataTable GetData()
+        {
+            return _data;
+        }
+
+        public void RefreshData(int rows)
+        {
+            _data.Rows.Clear();
+            var random = new Random();
+
+            for (int i = 0; i < rows; i++)
+            {
+                _data.Rows.Add(random.Next(1, 255), random.NextDouble() >= 0.8);
+            }
+
+            DataChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
